29e461b96a9f71c48a44ddc34e9bccf7f9233782
Leo <leoshch@gmail.com>
2014-06-22 13:55:23 +0800

    Update pptpd.sh

--- pptpd.sh
+++ pptpd.sh
@@ -22,31 +22,32 @@
 #Version: 0.2
 #Ripped from https://gist.githubusercontent.com/yinhm/666241/raw/e8f3030a9e7066b8deb0a3d9ec761360e2d94227/pptpd.sh
 
+# Clean the old config
 yum remove -y pptpd ppp
 iptables --flush POSTROUTING --table nat
 iptables --flush FORWARD
 rm -rf /etc/pptpd.conf
 rm -rf /etc/ppp
 
-
-yum -y install rpm-build gcc
+# Install ppp  
 yum -y install ppp
 
+# Install pptd
 mkdir ~/src
 cd ~/src
-wget http://www.bradiceanu.net/files/pptpd-1.3.4-1.fc12.src.rpm
+wget http://poptop.sourceforge.net/yum/stable/packages/pptpd-1.4.0-1.el6.x86_64.rpm
+rpm -Uhv pptpd*.rpm
 
-rpmbuild --rebuild pptpd-1.3.4-1.fc12.src.rpm
-rpm -i ../rpmbuild/RPMS/i386/pptpd-1.3.4-1.amzn1.i386.rpm
-
+# Config /etc/pptpd.conf, /etc/sysctl.conf and /etc/ppp/options.pptpd
 sed -i 's/^logwtmp/#logwtmp/g' /etc/pptpd.conf
+echo "localip 192.168.240.1" >> /etc/pptpd.conf
+echo "remoteip 192.168.240.2-100" >> /etc/pptpd.conf
 
 sed -i 's/^net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
 sysctl -p
-echo "localip 192.168.240.1" >> /etc/pptpd.conf
-echo "remoteip 192.168.240.2-100" >> /etc/pptpd.conf
-echo "ms-dns 172.16.0.23" >> /etc/ppp/options.pptpd
+
 echo "ms-dns 8.8.8.8" >> /etc/ppp/options.pptpd
+echo "ms-dns 8.8.4.4" >> /etc/ppp/options.pptpd
 
 pass=`openssl rand 8 -base64`
 if [ "$1" != "" ]
